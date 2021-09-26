e3b97c697b06063485d6ec65dda91b63d0d17173
Jenny Juyoung Jung <84524297+jennyjjung@users.noreply.github.com>
2021-09-26 17:10:50 -0400

    Root page, CSS

--- ContainerList/src/main/java/ca/sheridancollege/jungjuyo/controllers/HomeController.java
+++ ContainerList/src/main/java/ca/sheridancollege/jungjuyo/controllers/HomeController.java
@@ -1,8 +1,15 @@
 package ca.sheridancollege.jungjuyo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	
+	@GetMapping
+	public String goHome() {
+		return "index.html";
+	}
+	
 }
