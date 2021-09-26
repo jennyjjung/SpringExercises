900d11c83324fe5bf2b3cb74ab36b210b96560c6
Jenny Juyoung Jung <84524297+jennyjjung@users.noreply.github.com>
2021-09-26 17:50:32 -0400

    Error page, controller

--- ContainerList/src/main/java/ca/sheridancollege/jungjuyo/controllers/HomeController.java
+++ ContainerList/src/main/java/ca/sheridancollege/jungjuyo/controllers/HomeController.java
@@ -2,14 +2,46 @@ package ca.sheridancollege.jungjuyo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ca.sheridancollege.jungjuyo.beans.Container;
+import ca.sheridancollege.jungjuyo.repositories.ContainerDepartment;
 
 @Controller
 public class HomeController {
 
 	
-	@GetMapping
+	@GetMapping("/")
 	public String goHome() {
 		return "index.html";
+	}
+	
+	@GetMapping("processContainer")
+	public String process(@RequestParam int id,
+						  @RequestParam String name,
+						  @RequestParam double volume) {
+		
+		try {
+			
+			Container container = new Container(id, name, volume);
+			if (volume <= 10) {
+				ContainerDepartment.books.add(container);
+			} else if (volume > 10 && volume <= 50) {
+				ContainerDepartment.electronics.add(container);
+			} else if (volume > 50) {
+				if (name.contains("crate"))
+					ContainerDepartment.furniture.add(container);
+				else
+					ContainerDepartment.bedding.add(container);
+			} 
+			
+		} catch (IllegalArgumentException ex){
+			
+			return "error.html";
+			
+		}
+		
+		return "output.html";
 	}
 	
 }
---
+++ ContainerList/src/main/java/ca/sheridancollege/jungjuyo/repositories/ContainerDepartment.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.jungjuyo.repositories;
+
+import java.util.ArrayList;
+import ca.sheridancollege.jungjuyo.beans.Container;
+
+public class ContainerDepartment {
+	
+	public static ArrayList<Container> books = new ArrayList<Container>();
+	public static ArrayList<Container> electronics = new ArrayList<Container>();
+	public static ArrayList<Container> furniture = new ArrayList<Container>();
+	public static ArrayList<Container> bedding = new ArrayList<Container>();
+
+}
